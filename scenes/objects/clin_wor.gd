097b5bf4dce7e6947ea75ac4fb3227ccb7221fc4
ehmenzala <ehmenzala@gmail.com>
2023-12-01 10:24:11 -0500

    Add main character movement and animations

---
+++ scenes/objects/clin_wor.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@onready var animationNode = get_node("AnimationPlayer")
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	animationNode.play("idle")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animationNode.play("jump")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	else:
+		get_node("AnimatedSprite2D").flip_h = false
+	if direction:
+		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animationNode.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animationNode.play("idle")
+	if velocity.y > 0:
+		animationNode.play("fall")
+	move_and_slide()
