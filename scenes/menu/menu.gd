c31b65816a99cfa65f15d62d0c0d565d5e371c4b
ehmenzala <ehmenzala@gmail.com>
2023-12-03 17:44:58 -0500

    Add menu background

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	Game.next_level = 1
+	Game.next_cinematic = 0
 
 func _on_button_pressed():
 	get_tree().quit()
