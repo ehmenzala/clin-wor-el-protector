8ca21ab342a8134af3bf307e176cb6a489c2eb85
ehmenzala <ehmenzala@gmail.com>
2023-12-02 23:30:28 -0500

    Link main menu with intro cinematic

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -6,10 +6,9 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_button_pressed():
 	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/cinematics/cinematic.tscn")
